ece3ed9015609f85ea47be61657914aedb50b048
DianaGC <garcapdia@hotmail.com>
2025-06-17 16:40:57 -0600

    Ejemplos Clase 2

---
+++ Clase2/ejemplo_busqueda/src/components/Song.jsx
@@ -0,0 +1,11 @@
+import './Song.css'
+export default function Song({id, title, artist, album}) {
+    return (
+        <div className="song-item">
+            <p>{id}</p>
+            <p className="song-title">{title}</p>
+            <p className="song-artist">{artist}</p>
+            <p className="song-album">{album}</p>
+        </div>
+    )
+}
