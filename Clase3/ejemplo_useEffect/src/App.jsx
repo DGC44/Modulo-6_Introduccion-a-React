08ad05c1ec3ec8a8c06d1137699170630c72b367
DianaGC <garcapdia@hotmail.com>
2025-06-17 16:53:43 -0600

    Ejemplos Clase 3

---
+++ Clase3/ejemplo_useEffect/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+import ComponentChild from './components/ComponentChild'
+
+function App() {
+  const [name, setName] = useState("Primero");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Se ejecutó el efecto!")
+  }, [name, count]) 
+
+  function handeClick() {
+    setCount(count + 1)
+    setName(`Tercero ${count}`)
+  }
+
+  return (
+    <div>
+      <h1>Hola {name}!</h1>
+      <button onClick={handeClick}>Cambiar Nombre</button>
+      <ComponentChild />
+    </div>
+  )
+}
+
+export default App
