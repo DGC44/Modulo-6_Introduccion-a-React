79999dd15fa59c0948f670d0db847d822e624722
DianaGC <garcapdia@hotmail.com>
2025-06-17 16:06:43 -0600

    Clase 1. Componentes

---
+++ Clase1/ejemplo_componente/src/App.jsx
@@ -0,0 +1,18 @@
+import { } from 'react'
+import './App.css'
+import Header from './componentes/Header'
+import Content from './componentes/Content'
+import Footer from './componentes/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default App
