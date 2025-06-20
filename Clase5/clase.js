814d619fcac4ad1c8ba45fe8d646727c1362a417
DianaGC <garcapdia@hotmail.com>
2025-06-19 18:26:41 -0600

    Clase 6

--- Clase5/clase.js
+++ Clase5/clase.js
@@ -1,19 +1,50 @@
 const condicion = true
 
-if (condicion) {
-    console.log('condicion verdadera')
+// If tradicional
+if(condicion) {
+    console.log("Imprime ésto si la condicion es verdadera")
 } else {
-    console.log('condicion falsa')
+    console.log("Imprime ésto si la condición es falsa")
 }
 
+// Operador ternario
 
-// Operador Ternario
+condicion ? console.log("Imprime ésto si la condicion es verdadera") : console.log("Imprime ésto si la condición es falsa")
 
-condicion ? console.log('condicion verdadera') : console.log('condicion falsa')
-
-const texto = condicion ? 'condicion es verdadera' : 'condicion es falsa'
+const texto = condicion ? "La condicion fue verdadera" : "La condición fue falsa"
 console.log(texto)
 
-let texto2 = 'condicion es falsa'
-texto2 = condicion || "condicion es verdadera"
+let texto2 = "La condicion fue verdadera"
+texto2 = condicion || "La condición fue falsa"
 console.log(texto2)
+
+
+const color = "blue"
+switch(color) {
+    case "black":
+        console.log("Your picked color is black")
+        break
+    case "red":
+        console.log("Your picked color is red")
+        break
+    case "blue":
+        console.log("Your picked color is blue")
+        break
+    case "green":
+        console.log("Your picked color is green")
+        break
+    default:
+        console.log("Your picked color is unknown")
+        break
+}
+
+// Lo mismo pero utilizando un objeto
+const results = {
+    "red": "Your picked color is red",
+    "black": "Your picked color is black",
+    "blue": "Your picked color is blue",
+    "green": "Your picked color is green",
+    "yellow": "Your picked color is yellow"
+}
+
+console.log(results[color])
---
+++ Clase5/ejemplo_children/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Component>
+      <h1>Etiqueta Hija</h1>
+      {/* Lorem ipsum con varios articles y sections */}
+      <article>
+        <h2>Etiqueta Hija 2</h2>
+        <p>Etiqueta Hija 3</p>
+      </article>
+    </Component>
+  )
+}
+
+export default App
